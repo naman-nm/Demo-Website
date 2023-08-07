696a985403f03f37f415762c8744c6b531f7e954
unknown <nmathur949@gmail.com>
2023-08-07 13:42:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElemArr = [overlay, navOpenBtn, navCloseBtn];
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 200 ? header.classList.add("active")
+    : header.classList.remove("active");
+})
